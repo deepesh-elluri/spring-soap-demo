e67ba9cd8ef62b3ac4829dc6b4c2b98d49543cab
deepesh <deepeshreddy.elluri@gmail.com>
2022-03-19 22:33:12 +0530

    initial commit

---
+++ src/test/java/com/example/soap/springsoapdemo/SpringSoapDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soap.springsoapdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSoapDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
